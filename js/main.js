dac85fd312ea27fb881233fd9a340bcee34ec660
Gisela Victoria <giselavictoria.v@gmail.com>
2021-06-18 19:09:33 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,195 @@
+let pantallaCarga = document.getElementById("pantallaCarga");
+let pantallaJuego = document.getElementById("pantallaJuego");
+
+let inputPlayer1 = document.getElementById("inputJugador1");
+let inputPlayer2 = document.getElementById("inputJugador2");
+let player1 = document.getElementById("player1");
+let player2 = document.getElementById("player2");
+
+let player1Score = document.getElementById("player1Score");
+let player2Score = document.getElementById("player2Score");
+
+let botonJugar = document.getElementById("botonJugar");
+
+let grillaContainer = document.querySelector(".grillaContainer");
+
+let jugadasPlayer1 = [];
+let jugadasPlayer2 = [];
+let scorePartidaPlayer1 = 0;
+let scorePartidaPlayer2 = 0;
+let scoreTotalPlayer1 = [];
+let scoreTotalPlayer2 = [];
+
+let posicionesGanadoras = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let ganador = false;
+let contadorJugadas = 0;
+let resultado;
+
+botonJugar.addEventListener("click", function () {
+  player1.innerHTML = inputPlayer1.value;
+  player2.innerHTML = inputPlayer2.value;
+  pantallaCarga.style.display = "none";
+  pantallaJuego.style.display = "block";
+});
+
+class Slot {
+  constructor(contenedor, slotId) {
+    this.contenedor = contenedor;
+    this.slotClicked = false;
+    this.slotId = slotId;
+  }
+
+  pintarRosa() {
+    this.contenedor.style.background = "pink";
+  }
+
+  pintarVerde() {
+    this.contenedor.style.background = "green";
+  }
+}
+
+const slots = [];
+
+for (let i = 0; i < 9; i++) {
+  slots.push(new Slot(document.getElementById("slot" + i), i));
+}
+
+slots.forEach((slot) => {
+  slot.contenedor.addEventListener("click", () => jugar(slot));
+});
+
+function jugar(slot) {
+  if (!ganador) {
+    if (
+      contadorJugadas === 0 ||
+      contadorJugadas === 2 ||
+      contadorJugadas === 4 ||
+      contadorJugadas === 6 ||
+      contadorJugadas === 8
+    ) {
+      if (!slot.slotClicked) {
+        slot.pintarRosa();
+        jugadasPlayer1.push(slot.slotId);
+        console.log(jugadasPlayer1);
+        busqueda(posicionesGanadoras, jugadasPlayer1);
+        slot.slotClicked = true;
+        contadorJugadas++;
+        if (resultado) {
+          ganador = true;
+          scorePartidaPlayer1++;
+          scoreTotalPlayer1.push(scorePartidaPlayer1);
+          console.log(scoreTotalPlayer1);
+          updateScore(player1Score, scorePartidaPlayer1);
+          console.log("gano player 1");
+        } else if (!ganador && contadorJugadas === 9) {
+          console.log("Es un empate");
+        }
+      } else if (slot.slotClicked) {
+        console.log("pinta otro");
+      }
+    } else if (
+      contadorJugadas === 1 ||
+      contadorJugadas === 3 ||
+      contadorJugadas === 5 ||
+      contadorJugadas === 7
+    ) {
+      if (!slot.slotClicked) {
+        slot.pintarVerde();
+        jugadasPlayer2.push(slot.slotId);
+        console.log(jugadasPlayer2);
+        busqueda(posicionesGanadoras, jugadasPlayer2);
+        slot.slotClicked = true;
+        contadorJugadas++;
+        if (resultado) {
+          ganador = true;
+          scorePartidaPlayer2++;
+          scoreTotalPlayer2.push(scorePartidaPlayer2);
+          console.log(scoreTotalPlayer2);
+          updateScore(player2Score, scorePartidaPlayer2);
+          console.log("gano player 2");
+        } else if (!ganador && contadorJugadas === 9) {
+          console.log("Es un empate");
+        }
+      } else if (slot.slotClicked) {
+        console.log("pinta otro");
+      }
+    }
+  } else if (ganador) {
+    console.log("entro al if del slots");
+    slot.contenedor.removeEventListener("click", () => jugar(slot));
+  }
+}
+
+function busqueda(arr1, arr2) {
+  for (var i = 0; i < arr1.length; i++) {
+    resultado = arr1[i].every((item) => arr2.includes(item));
+    console.log(resultado);
+    if (resultado === true) {
+      break;
+    }
+  }
+}
+
+function updateScore(player, score) {
+  player.innerHTML = score;
+}
+
+/*slots.forEach((slot, i) => {
+  slot.contenedor.addEventListener("click", function () {
+    if (
+      contadorJugadas === 0 ||
+      contadorJugadas === 2 ||
+      contadorJugadas === 4 ||
+      contadorJugadas === 6 ||
+      contadorJugadas === 8
+    ) {
+      if (!slot.slotClicked) {
+        slot.pintarRosa();
+        jugadasPlayer1.push(slot.slotId);
+        console.log(jugadasPlayer1);
+        busqueda(posicionesGanadoras, jugadasPlayer1);
+        slot.slotClicked = true;
+        contadorJugadas++;
+        if (resultado === true) {
+          ganador = true;
+          console.log("gano player 1");
+        }
+      } else if (slot.slotClicked) {
+        console.log("pinta otro");
+      }
+    } else if (
+      contadorJugadas === 1 ||
+      contadorJugadas === 3 ||
+      contadorJugadas === 5 ||
+      contadorJugadas === 7
+    ) {
+      if (!slot.slotClicked) {
+        slot.pintarVerde();
+        jugadasPlayer2.push(slot.slotId);
+        console.log(jugadasPlayer2);
+        busqueda(posicionesGanadoras, jugadasPlayer2);
+        slot.slotClicked = true;
+        contadorJugadas++;
+        if (resultado === true) {
+          ganador = true;
+          console.log("gano player 2");
+        }
+      } else if (slot.slotClicked === false) {
+        console.log("pinta otro");
+      }
+    }
+    if (ganador === true) {
+      console.log("ya hay un ganador");
+    }
+  });
+});*/
